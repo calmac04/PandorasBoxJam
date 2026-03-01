f15650e3cca39ac5776682d55c747b9c8913b1bd
Dylan <dylanjhamilton@outlook.com>
2026-03-01 11:39:38 +0000

    Added WIP dialogue system

--- PandorasBox/Assets/DylAssets/Scripts/BattleCommandScript.cs
+++ PandorasBox/Assets/DylAssets/Scripts/BattleCommandScript.cs
@@ -7,8 +7,18 @@ public class BattleCommandScript : MonoBehaviour
     public GameObject offenseChildren;
     public GameObject defenseChildren;
     public GameObject utilityChildren;
+    public GameObject dialogueBox;
+    TMP_Text dialogue;
     List<GameObject> selectors = new List<GameObject>();
     List<bool> selectorVisibility = new List<bool> { true, true, true };
+
+    List<string> lines = new List<string>
+    {
+        "What will you do?", //Placeholder text
+        "Deal moderate damage to the enemy.", //Basic Attack
+        "Defend against enemy attacks this turn.", //Basic Defend
+        "Restore your own health.", //Basic Heal
+    };
 
     //Will either activate or deactivate the selector children and corresponding selector children
     public void SelectorSwitch(int selector)
@@ -57,6 +67,10 @@ public class BattleCommandScript : MonoBehaviour
         {
             DeactivateSelector(i);
         }
+
+        GameObject tempDia = dialogueBox.transform.GetChild(1).gameObject;
+        dialogue = tempDia.GetComponent<TMP_Text>();
+        dialogue.text = lines[0];
     }
 
     // Update is called once per frame
