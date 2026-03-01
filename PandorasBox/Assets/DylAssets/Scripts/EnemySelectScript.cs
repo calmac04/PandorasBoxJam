cb79dece345615c83e70b6fa9b12a6184b2eb527
Dylan <dylanjhamilton@outlook.com>
2026-03-01 13:33:03 +0000

    Enemy UI Mockup 2

---
+++ PandorasBox/Assets/DylAssets/Scripts/EnemySelectScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySelectScript : MonoBehaviour
+{
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public GameObject enemy4;
+
+    public void ActivateEnemies()
+    {
+        Button temp = enemy1.GetComponent<Button>();
+        temp.interactable = true;
+        temp = enemy2.GetComponent<Button>();
+        temp.interactable = true;
+        temp = enemy3.GetComponent<Button>();
+        temp.interactable = true;
+        temp = enemy4.GetComponent<Button>();
+        temp.interactable = true;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
